b1958b61c0a4ff1035e7c94fd60aca198810bcb9
Derek Clapham <derek.clapham@fotomerchant.com>
2016-12-07 16:15:03 +1100

    Added reflection to ThemeCompilerPass to address changes made in symfony/symfony (https://github.com/symfony/symfony/commit/2d4268936a05254a4e8b2d1ed7d2fc08881a098b)

--- DependencyInjection/Compiler/ThemeCompilerPass.php
+++ DependencyInjection/Compiler/ThemeCompilerPass.php
@@ -29,7 +29,22 @@ class ThemeCompilerPass implements CompilerPassInterface
         }
 
         $twigFilesystemLoaderDefinition = $container->getDefinition('twig.loader.filesystem');
+        $reflectionKlass = new \ReflectionClass($twigFilesystemLoaderDefinition->getClass());
         $twigFilesystemLoaderDefinition->setClass($container->getParameter('liip_theme.filesystem_loader.class'));
-        $twigFilesystemLoaderDefinition->addArgument(new Reference('liip_theme.active_theme'));
+
+        /*
+         * symfony/symfony commit #2d42689 introduced an optional 3rd argument to the constructor of
+         * Symfony\Bundle\TwigBundle\Loader\FilesystemLoader. It is injected during the compiler pass of the Symfony
+         * Twig bundle.
+         *
+         * As such the code below determines the number of arguments found in the twig FilesystemLoader constructor
+         * using reflection and either replaces the 3rd argument with an instance of ActiveTheme or adds the ActiveTheme
+         * as a new argument
+         */
+        if ($reflectionKlass->getConstructor()->getNumberOfParameters() == 3) {
+            $twigFilesystemLoaderDefinition->replaceArgument(2, new Reference('liip_theme.active_theme'));
+        } else {
+            $twigFilesystemLoaderDefinition->addArgument(new Reference('liip_theme.active_theme'));
+        }
     }
 }
